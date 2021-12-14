a7b469bba8b431c1de3774727e9bd3fb215f2369
贾宇辰 <1260850094@qq.com>
2021-12-14 16:43:12 +0800

    2021.12.14 贾宇辰
    更新了店铺的星级显示和距离显示

--- cloudfunctions/get_store_list/index.js
+++ cloudfunctions/get_store_list/index.js
@@ -4,15 +4,18 @@ const cloud = require('wx-server-sdk')
 cloud.init({
   env:"cloud1-6gtiz48ybf23c5c5"
 })
+const _ = cloud.database().command
+var $ = cloud.database().command.aggregate
 // 云函数入口函数
 exports.main = async (event, context) => {
   return cloud.database().collection("store_detail_list").aggregate() //选择我的商店表
           .lookup({
             from:"distance", //把distance表关联上
-            localField: 'shop_id', //store表的关联字段
-            foreignField: 'shop_id', //dis表的关联字段
+            let:{},
+            pipeline:[]
             as: 'store_list' //匹配的结果作为store_list相当于起个别名
-          }).end({
+          })
+          .end({
             success:function(res){
               return res;
             },
--- miniprogram/pages/store_detail_list/store_detail_list.js
+++ miniprogram/pages/store_detail_list/store_detail_list.js
@@ -20,26 +20,29 @@ Page({
    */
   onLoad: function (options) {
     let that = this
-    wx.cloud.callFunction(
-      {
-        name:'get_store_list',
-        success:function(res){
-          console.log(res.result)
-      },
-      fail:console.error
-      })
-    // db.collection('store_detail_list').orderBy("store_satisfaction", "desc").get({
-    //   success:function(res){
-    //     console.log('获取商店信息成功',res)
-    //     that.setData({
-    //       store: res.data,
-    //     })
-        
+    // wx.cloud.callFunction(
+    //   {
+    //     name:'get_store_list',
+    //     success:function(res){
+    //       console.log('联表成功',res.result)
+    //       that.setData({
+    //         store: res.result.list,
+    //       })
     //   },
-    //   fail:function(res){
-    //     console.log('获取商店信息失败',res)
-    //   }
-    // })
+    //   fail:console.error
+    //   })
+    db.collection('store_detail_list').orderBy("store_satisfaction", "desc").get({
+      success:function(res){
+        console.log('获取商店信息成功',res)
+        that.setData({
+          store: res.data,
+        })
+        
+      },
+      fail:function(res){
+        console.log('获取商店信息失败',res)
+      }
+    })
   },
 
   /**
@@ -49,6 +52,18 @@ Page({
 
   },
   radioChange: function(e) {
+      // wx.cloud.callFunction(
+      //   {
+      //     name:'get_store_list',
+      //     success:function(res){
+      //       console.log('联表成功',res.result)
+      //       that.setData({
+      //         store: res.result.list,
+      //       })
+      //   },
+      //   fail:console.error
+      //   })
+      // },
     let that = this
     console.log('排序方式发生改变，目前的排序方式是：' + e.detail.value)
     if (e.detail.value == 1){
